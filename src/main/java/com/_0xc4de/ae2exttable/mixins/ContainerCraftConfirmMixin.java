36dd6ee05485b2103221cab9b3ef0255a228d097
Circulate233 <a18966009058@163.com>
2025-01-29 21:36:01 +0800

    Fixed the issue where the server synthesis process failed to return to the previous GUI interface. Fixed the problem where pressing the cancel button in the GuiCraftingStatus interface unexpectedly returned to the previous GUI.

--- src/main/java/com/_0xc4de/ae2exttable/mixins/ContainerCraftConfirmMixin.java
+++ src/main/java/com/_0xc4de/ae2exttable/mixins/ContainerCraftConfirmMixin.java
@@ -3,24 +3,19 @@ package com._0xc4de.ae2exttable.mixins;
 import appeng.api.parts.IPart;
 import appeng.container.AEBaseContainer;
 import appeng.container.implementations.ContainerCraftConfirm;
-import com._0xc4de.ae2exttable.client.gui.AE2ExtendedGUIs;
+import appeng.container.interfaces.IInventorySlotAware;
 import com._0xc4de.ae2exttable.client.gui.PartGuiHandler;
 import com._0xc4de.ae2exttable.interfaces.ITerminalGui;
-import com._0xc4de.ae2exttable.network.ExtendedTerminalNetworkHandler;
-import com._0xc4de.ae2exttable.network.packets.PacketSwitchGui;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.tileentity.TileEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value= ContainerCraftConfirm.class, remap=false)
 public class ContainerCraftConfirmMixin extends AEBaseContainer {
-
-    @Unique
-    public AE2ExtendedGUIs aE2ExtendedCraftingTable$extendedOriginalGui;
 
     public ContainerCraftConfirmMixin(InventoryPlayer ip, TileEntity myTile, IPart myPart) {
       super(ip, myTile, myPart);
@@ -32,22 +27,14 @@ public class ContainerCraftConfirmMixin extends AEBaseContainer {
         if (te != null) {
             IPart terminal = PartGuiHandler.getPartFromWorld(te.getWorld(), te.getPos(), this.getOpenContext().getSide());
             if (terminal instanceof ITerminalGui t) {
-                aE2ExtendedCraftingTable$extendedOriginalGui = t.getGuiType();
-                ExtendedTerminalNetworkHandler.instance()
-                    .sendToServer(new PacketSwitchGui(
-                        aE2ExtendedCraftingTable$extendedOriginalGui));
+                PartGuiHandler.openGUI(t.getGuiType(), this.getInventoryPlayer().player, te.getPos(), this.getOpenContext().getSide());
                 ci.cancel();
             }
+        } else if (this.obj.getItemStack().getItem() instanceof ITerminalGui t) {
+            IInventorySlotAware i = ((IInventorySlotAware) this.obj);
+            EntityPlayer player = this.getInventoryPlayer().player;
+            PartGuiHandler.openWirelessTerminalGui(this.obj.getItemStack(), i.getInventorySlot(), i.isBaubleSlot(), player.world, player, t.getGuiType());
+            ci.cancel();
         }
-        if (this.obj != null) {
-            if (this.obj.getItemStack().getItem() instanceof ITerminalGui t) {
-                aE2ExtendedCraftingTable$extendedOriginalGui = t.getGuiType();
-                ExtendedTerminalNetworkHandler.instance()
-                    .sendToServer(new PacketSwitchGui(
-                        aE2ExtendedCraftingTable$extendedOriginalGui));
-                ci.cancel();
-            }
-        }
-
     }
 }
