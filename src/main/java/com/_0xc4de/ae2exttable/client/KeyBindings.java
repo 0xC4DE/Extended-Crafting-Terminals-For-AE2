07b215d5cfd3d79dba605ceac279387dc404be79
0xC4DE <0xc4de@0xc4de.dev>
2025-04-10 11:18:27 -0500

    Keybindings (#10)

---
+++ src/main/java/com/_0xc4de/ae2exttable/client/KeyBindings.java
@@ -0,0 +1,25 @@
+package com._0xc4de.ae2exttable.client;
+
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraftforge.client.settings.KeyConflictContext;
+import net.minecraftforge.client.settings.KeyModifier;
+import org.lwjgl.input.Keyboard;
+
+import static com._0xc4de.ae2exttable.proxy.ClientProxy.KEY_CATEGORY;
+
+public enum KeyBindings {
+    basic(new KeyBinding("key.open_basic_extended_wireless_crafting_terminal", KeyConflictContext.UNIVERSAL, KeyModifier.NONE, Keyboard.KEY_NONE, KEY_CATEGORY)),
+    advanced(new KeyBinding("key.open_advanced_extended_wireless_crafting_terminal", KeyConflictContext.UNIVERSAL, KeyModifier.NONE, Keyboard.KEY_NONE, KEY_CATEGORY)),
+    elite(new KeyBinding("key.open_elite_extended_wireless_crafting_terminal", KeyConflictContext.UNIVERSAL, KeyModifier.NONE, Keyboard.KEY_NONE, KEY_CATEGORY)),
+    ultimate(new KeyBinding("key.open_ultimate_extended_wireless_crafting_terminal", KeyConflictContext.UNIVERSAL, KeyModifier.NONE, Keyboard.KEY_NONE, KEY_CATEGORY));
+
+    private KeyBinding binding;
+
+    KeyBindings(KeyBinding keyBinding) {
+        this.binding = keyBinding;
+    }
+
+    public KeyBinding get() {
+        return this.binding;
+    }
+}
---
+++ src/main/java/com/_0xc4de/ae2exttable/network/packets/PacketOpenWirelessGui.java
@@ -0,0 +1,70 @@
+package com._0xc4de.ae2exttable.network.packets;
+
+import appeng.api.networking.security.IActionHost;
+import appeng.container.AEBaseContainer;
+import appeng.container.ContainerOpenContext;
+import appeng.container.interfaces.IInventorySlotAware;
+import appeng.core.sync.network.INetworkInfo;
+import appeng.util.Platform;
+import baubles.api.BaublesApi;
+import com._0xc4de.ae2exttable.client.gui.AE2ExtendedGUIs;
+import com._0xc4de.ae2exttable.client.gui.PartGuiHandler;
+import com._0xc4de.ae2exttable.items.ItemRegistry;
+import com._0xc4de.ae2exttable.network.ExtendedTerminalPacket;
+import com._0xc4de.ae2exttable.network.PacketHandler;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+
+
+public class PacketOpenWirelessGui extends ExtendedTerminalPacket {
+
+  private final AE2ExtendedGUIs gui;
+
+  public PacketOpenWirelessGui(final ByteBuf stream) {
+    this.gui = AE2ExtendedGUIs.values()[stream.readInt()];
+  }
+
+  public PacketOpenWirelessGui(final AE2ExtendedGUIs gui) {
+    this.gui = gui;
+    final ByteBuf data = Unpooled.buffer();
+    data.writeInt(this.getGuiPacketID());
+    data.writeInt(gui.ordinal());
+
+    this.configureWrite(data);
+  }
+
+  public int getGuiPacketID() {
+    return PacketHandler.PacketTypes.getID(this.getClass()).ordinal();
+  }
+
+  @Override
+  public void serverPacketData(final INetworkInfo manager,
+                               final ExtendedTerminalPacket packet,
+                               final EntityPlayer player) {
+        NonNullList<ItemStack> inventory = player.inventory.mainInventory;
+        ItemStack guiItem = new ItemStack(ItemRegistry.partByGuiType(this.gui));
+        for (int i = 0; i < inventory.size(); i++) {
+            ItemStack is = inventory.get(i);
+            if (Platform.itemComparisons().isEqualItemType(is, guiItem)) {
+                openGui(is, i, player, false);
+                return;
+            }
+        }
+        if (Platform.isModLoaded("baubles")) {
+            for (int i = 0; i< BaublesApi.getBaublesHandler(player).getSlots(); i++) {
+                ItemStack is = BaublesApi.getBaublesHandler(player).getStackInSlot(i);
+                if (Platform.itemComparisons().isEqualItemType(is, guiItem)) {
+                  openGui(is, i, player, true);
+                  return;
+                }
+            }
+        }
+    }
+
+    private void openGui(ItemStack itemStack, int slotIndex, EntityPlayer player, boolean isBauble) {
+        PartGuiHandler.openWirelessTerminalGui(itemStack, slotIndex, isBauble, player.world, player, this.gui);
+    }
+}
